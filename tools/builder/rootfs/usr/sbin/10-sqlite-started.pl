175fb3f0a63d84d2730be59839b2f708a6919f57
ralf <r.hofmann@sap.com>
2017-02-13 13:38:50 +0100

    perl sqlite

---
+++ tools/builder/rootfs/usr/sbin/10-sqlite-started.pl
@@ -0,0 +1,28 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use feature qw( say );
+
+use DBI;
+
+my $dbfile = '/mnt/geoip/networks.db';
+
+use DBD::SQLite::Constants qw/:file_open/;
+
+my $dbh = DBI->connect("dbi:SQLite:$dbfile", undef, undef, {
+    sqlite_open_flags => SQLITE_OPEN_READONLY,
+  });
+
+my $sth = $dbh->prepare(
+   'SELECT network FROM IPv4 WHERE country_id = ?'
+);
+$sth->execute(2921044);
+
+while (my @row = $sth->fetchrow_array) {
+   print "network: $row[0]\n";
+}
+
+$dbh->disconnect;
+
+say "query done";
