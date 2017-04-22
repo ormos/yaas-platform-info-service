d4d6902a5ac58f4e1d24d449cf9cf351cfcc908c
ralf <r.hofmann@sap.com>
2017-04-22 20:51:47 +0200

    Added supplements support

--- rootfs/var/nginx/lua/market_entity.lua
+++ rootfs/var/nginx/lua/market_entity.lua
@@ -4,7 +4,8 @@ local base_url  = utils.base_url()
 local server_id = ngx.md5(base_url)
 
 -- check if we got a request with ../<country>/<resource> pattern
-local country, resource = ngx.unescape_uri(ngx.var.request_uri):match('^.*/markets/(.+)/(.+)$')
+local country, resource = ngx.unescape_uri(ngx.var.request_uri):match('^.*/markets/(.[^/]+)/(.+)$')
+
 if country == nil and resource == nil then
     -- we got an request with ../<country> pattern
     country = ngx.unescape_uri(ngx.var.request_uri):match('^.*/markets/(.+)$')
@@ -33,13 +34,23 @@ end
 if resource ~= nil then
     local market_data = cjson.decode(market)
 
-    -- see if we have a resource link for the specific market
-    if market_data ~= nil and market_data['links'][resource] ~= nil then
-        ngx.log(ngx.INFO, 'Redirect resource='..resource..' at market='..country..' to URL: "'..market_data['links'][resource]..'"')
-        ngx.redirect(market_data['links'][resource], ngx.HTTP_MOVED_TEMPORARILY);
-    else
-        ngx.exit(404)
+    if market_data ~= nil then
+        -- see if we have a resource link for the specific market
+        if market_data['links'][resource] ~= nil then
+            ngx.log(ngx.INFO, 'Redirect resource='..resource..' at market='..country..' to URL: "'..market_data['links'][resource]..'"')
+            ngx.redirect(market_data['links'][resource], ngx.HTTP_MOVED_TEMPORARILY)
+            return
+        else
+            -- check for special supplements handling
+            local element, name = resource:match('^([^/]+)/(.+)$')
+            if element == 'supplements' and market_data['supplements'][name] ~= nil then
+                ngx.print(cjson.encode(market_data['supplements'][name]))
+                return
+            end
+        end
     end
+
+    ngx.exit(404)
 else
     ngx.print(market)
 end
